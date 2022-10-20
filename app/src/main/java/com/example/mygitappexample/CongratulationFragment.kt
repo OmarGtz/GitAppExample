3bc9182df1572ea652840cdcdc74a8dd038adc07
omar.gutierrez <omar.gutierrez@kavak.com>
2022-10-19 20:51:21 -0500

    Add navigation graph

--- app/src/main/java/com/example/mygitappexample/CongratulationFragment.kt
+++ app/src/main/java/com/example/mygitappexample/CongratulationFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class CongratulationFragment:Fragment() {
 
@@ -15,4 +17,12 @@ class CongratulationFragment:Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_congratulation,container,false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.button_congr).setOnClickListener {
+            findNavController().navigate(R.id.action_congratulationFragment_to_aboutFragment)
+        }
+    }
+
 }
